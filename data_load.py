21770151970cc4473dbe47de26eac00619e7fd48
Tony Bailey <baileytj@mail.uc.edu>
2019-02-14 20:53:24 -0500

    the main code is nearly done. I just need to add the required graphs

--- data_load.py
+++ data_load.py
@@ -1,32 +1,24 @@
 import os
-import sys
 import random
 
 class Node:
-    def __init__(self, name, group):
+    def __init__(self, name):
         self.name = name
-        self.group = group
         self.nbrs = {}  # constant time lookup for membership/weight
 
 class Graph:
     def __init__(self):
       self.nodes = {}  # constant time lookup for membership/weight
 
-    def add_node(self, name):
-        if name <= self.num_cells//2:
-            group = 1
-        else:
-            group = 0
-        self.nodes[name] = Node(name, group)
     def add_edge(self, edge):
         #edge is a tuple of length 1, 2
         # add 0th entry to dict if it currently doesn't exist
         if edge[0] not in self.nodes:
-            self.add_node(edge[0])
+            self.nodes[edge[0]] = Node(edge[0])
         if len(edge) > 1:
             # add 1st entry to dict if it currently doesn't exist
             if edge[1] not in self.nodes:
-                self.add_node(edge[1])
+                self.nodes[edge[1]] = Node(edge[1])
             # add 0->1 edge
             if edge[0] in self.nodes[edge[1]].nbrs:
                 # if it's a repeat, add 1 to "weight"
@@ -43,18 +35,62 @@ class Graph:
                 #else, create connection
                 self.nodes[edge[0]].nbrs[edge[1]] = 1
 
+class GroupLst:
+    def __init__(self, graph, num_cells):
+      self.graph = graph  # pointer to adjacency list
+      self.num_cells = num_cells
+      V0 = [0]*(self.num_cells//2)
+      V1 = [1]*(self.num_cells//2)
+      self.V = V0 + V1
+      random.shuffle(self.V)
+      self.init_cost()
+
+    def perturb(self):
+        # randomly switch two cells
+        A = random.randint(0,self.num_cells-1)
+        chosen = False
+        while not chosen:
+            B = random.randint(0,self.num_cells-1)
+            if B == A or self.V[B] == self.V[A]:
+                continue
+            else:
+                chosen = True
+        self.V[A], self.V[B] = int(not(self.V[A])), int(not(self.V[B]))
+
+        self.update_cost(A+1, B+1)
+        self.update_cost(B+1, A+1)
+
+    def init_cost(self):
+        cost = 0
+        for i, n in enumerate(self.V):
+            # Find 0 nodes w/ connections in 1
+            if n == 0:
+                if i+1 in self.graph.nodes:
+                    for nbr in self.graph.nodes[i+1].nbrs:
+                        if self.V[nbr-1] == 1:
+                            cost += self.graph.nodes[i+1].nbrs[nbr]
+        self.cost = cost
+
+    def update_cost(self, A, B):
+        gA= self.V[A-1]
+        if A in self.graph.nodes:
+            for nbr in self.graph.nodes[A].nbrs:
+                if self.V[nbr-1] == gA and nbr != (B):
+                    self.cost = self.cost - self.graph.nodes[A].nbrs[nbr]
+                elif self.V[nbr-1] != gA and nbr != (B):
+                    self.cost = self.cost + self.graph.nodes[A].nbrs[nbr]
+
 
 def data_load(folder, fn):
 
     with open(os.path.join(folder, fn)) as f:
         content = f.readlines()
-
+        f.close()
+        
+    num_cells, num_nets = int(content[0][:-1]), int(content[1][:-1])
     graph = Graph()
 
-    graph.num_cells = int(content[0][:-1])
-    graph.num_nets = int(content[1][:-1])
-
-    print(graph.num_cells, graph.num_nets)
+    print(num_cells, num_nets)
     if folder == "examples":
         # skip last row b/c it's a return char
         for row in content[2:-1]:
@@ -76,21 +112,4 @@ def data_load(folder, fn):
 
             graph.add_edge(edge)
 
-    for node in graph.nodes.values():
-        print(node.name, [x for x in node.nbrs], [x for x in node.nbrs.values()])
-
-    return graph
-
-
-
-
-
-def main():
-    #folder, fn = "Benchmarks", "b_100_500"
-
-    folder, fn = "examples", "bench_2.net"
-    data_load(folder, fn)
-
-
-if __name__ == "__main__":
-    main()
+    return graph, num_cells, num_nets
--- main.py
+++ main.py
@@ -1,5 +1,100 @@
-import data_load
+from data_load  import data_load, GroupLst
+import math
+import random
+import copy
+import time
+import os
 
+def acceptMove(delCost, T, num_cells):
+    if delCost < 0:
+        return True
+    else:
+        boltz = math.exp((-(delCost*num_cells))/(T))
+        r = random.uniform(0, 1)
+        if r < boltz:
+            return True
+        else:
+            return False
+
+def coolDown(T, params):
+    return T * params["Trate"]
+
+def heatUp(T, params):
+    T = params["Theat"]
+    params["Theat"] = params["factor"] * params["Theat"]
+    return T, params
+
+def simulatedAnnealing(graph, num_cells, params):
+    curSol, nextSol = GroupLst(graph, num_cells), GroupLst(graph, num_cells)
+    bestSol = copy.deepcopy(curSol)
+    T = params["T0"]
+    last_time, total_time = time.time(), time.time()
+    cntr, tol_cntr = 0, 0
+    while T > params["Tfreeze"]:
+        for i in range(params["num_moves_per_T"]):
+
+            nextSol.V, nextSol.cost = list(curSol.V), curSol.cost
+            nextSol.perturb()
+            delCost = nextSol.cost - curSol.cost
+
+            if acceptMove(delCost, T, num_cells):
+                curSol.V, curSol.cost = list(nextSol.V), nextSol.cost
+                tol_cntr = 0
+            else:
+                tol_cntr += 1
+            if curSol.cost < bestSol.cost:
+                bestSol.V, bestSol.cost = list(curSol.V), curSol.cost
+
+        cntr += 1
+        print("{} T: {} curCost: {}".format(cntr, T, curSol.cost))
+        print('T took {:0.3f} seconds'.format(time.time()-last_time))
+
+        last_time = time.time()
+        if tol_cntr < params["tolerance"] or params["Theat"] < params["Tfreeze"]:
+            T = coolDown(T, params)
+        elif params["Theat"] > params["Tfreeze"]:
+            # condition prevents heatup if it actually cools system down.
+            T, params = heatUp(T, params)
+            tol_cntr = 0
+
+
+    print("Best Cost: {}, temp iteration {}".format(bestSol.cost, cntr))
+    print("Total Time {:0.3f} seconds".format(time.time()-total_time))
+    return bestSol
+
+def writeResults(solution, fn):
+    f = open(os.path.join("Results", "R_" + fn), "w")
+    newline = str(solution.cost)+ "\n"
+    f.write(newline)
+    A, B = "", ""
+    for i, n in enumerate(solution.V):
+        if n == 0:
+            A += (str(i+1) + " ")
+        else:
+            B += (str(i+1) + " ")
+
+    f.write(A+"\n")
+    f.write(B+"\n")
+    f.close()
+
+
+
+
+def main():
+    folder, header, fn = "Benchmarks", "b_", "250000_1000000"
+    #folder, header, fn = "examples", "bench_", "16.net"
+    graph, num_cells, num_nets = data_load(folder, header + fn)
+
+    nmpt = int(0.02*num_nets+10)
+    print("num moves per T {}".format(nmpt))
+
+    params = {"T0": 10000, "Tfreeze": 0.01, "num_moves_per_T": nmpt,
+              "Trate":0.99, "Theat": 5000, "factor": 0.5, "tolerance": nmpt*100}  # k=1, so it won't be considered
+
+
+    solution = simulatedAnnealing(graph, num_cells, params)
+    writeResults(solution, fn)
+    print("num moves per T {}".format(nmpt))
 
 
 if __name__ == "__main__":
